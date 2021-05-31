95b8785271ccec5470b3bed253ce6f878450bf7d
zhongmou <294879521@qq.com>
2021-05-31 16:08:31 +0800

    feat:初始化项目

---
+++ src/com/zm/easyjira/window/TaskListWindow.java
@@ -0,0 +1,13 @@
+package com.zm.easyjira.window;
+
+import javax.swing.*;
+
+/**
+ * @author 仲谋
+ * @description:
+ * @date 2021/5/31
+ **/
+public class TaskListWindow {
+    private JTable table1;
+    private JButton button1;
+}
